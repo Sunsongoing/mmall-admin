b580c4d3e7b034fdac74aa61d277ab995fb62a2d
Sunsongoing <zjl_drong@163.com>
2018-12-28 13:52:28 +0800

    修复 url 无法访问

--- src/service/order-service.jsx
+++ src/service/order-service.jsx
@@ -3,22 +3,22 @@ import MUtil from 'util/currency.jsx';
 const _util = new MUtil();
 
 class Order {
-    // 获取商品列表
+    // 获取订单列表
     getOrderList(listParam) {
         let url = '',
             data = {};
         if (listParam.listType === 'list') {
-            url = '/manage/order/list';
+            url = _util.getServerUrl('/manage/order/list');
             data.pageNum = listParam.pageNum;
         } else if (listParam.listType === 'searchType') {
-            url = '/manage/order/search';
+            url = _util.getServerUrl('/manage/order/search');
             data.pageNum = listParam.pageNum;
             data.orderNo = listParam.orderNo;
         }
         return _util.request({
             type: 'post',
             data: data,
-            url: _util.getServerUrl(url)
+            url: url
         });
     }
     // 获取订单详情
